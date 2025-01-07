0b7f6cebe6e1f450e7a5d5554a894fc40141b15c
Alexander Medvednikov <alexander@medvednikov.com>
2025-01-07 23:48:01 +0300

    nice

--- vlib/v/checker/fn.v
+++ vlib/v/checker/fn.v
@@ -1753,14 +1753,18 @@ fn (mut c Checker) fn_call(mut node ast.CallExpr, mut continue_check &bool) ast.
 				call_arg.pos)
 		}
 		// Warn about automatic (de)referencing, which will be removed soon.
+		/*
 		if func.language != .c && !c.inside_unsafe && c.pref.experimental && c.fileis('a.v')
-			&& arg_typ.nr_muls() != param.typ.nr_muls() && !(!call_arg.is_mut && !param.is_mut)
+			&& arg_typ.nr_muls() != param.typ.nr_muls() // && !(!call_arg.is_mut && !param.is_mut)
 			&& param.typ !in [ast.byteptr_type, ast.charptr_type, ast.voidptr_type, ast.nil_type] {
+			println('1=${call_arg.is_mut} 2=${param.is_mut}')
 			c.warn('KEKW', call_arg.pos)
 		}
+		*/
 		if func.language != .c && !c.inside_unsafe && arg_typ.nr_muls() != param.typ.nr_muls()
-			&& !(call_arg.is_mut && param.is_mut) && !(!call_arg.is_mut && !param.is_mut)
-			&& param.typ !in [ast.byteptr_type, ast.charptr_type, ast.voidptr_type, ast.nil_type] {
+			&& !(call_arg.is_mut && param.is_mut)
+			&& param.typ !in [ast.byteptr_type, ast.charptr_type, ast.voidptr_type, ast.nil_type] //&& !(!call_arg.is_mut && !param.is_mut)
+		  {
 			c.warn('automatic referencing/dereferencing is deprecated and will be removed soon (got: ${arg_typ.nr_muls()} references, expected: ${param.typ.nr_muls()} references)',
 				call_arg.pos)
 		}
