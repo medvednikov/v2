02d337fbf451e0078738631b7f694b8b6c1205cc
Alexander Medvednikov <alexander@medvednikov.com>
2025-03-17 18:21:40 +0300

    frees

--- vlib/v/slow_tests/valgrind/string_all_methods.v
+++ vlib/v/slow_tests/valgrind/string_all_methods.v
@@ -1,6 +1,13 @@
 fn main() {
-	'Hello World'.wrap(width: 10)
-	'Hello World'.wrap(width: 10, end: '<linea-break>')
-	'The V programming language'.wrap(width: 20, end: '|')
-	'Hello, my name is Carl and I am a delivery'.wrap(width: 20)
+	s := 'Hello World'.wrap(width: 10)
+	s.free()
+
+	s2 := 'Hello World'.wrap(width: 10, end: '<linea-break>')
+	s2.free()
+
+	s3 := 'The V programming language'.wrap(width: 20, end: '|')
+	s3.free()
+
+	s4 := 'Hello, my name is Carl and I am a delivery'.wrap(width: 20)
+	s4.free()
 }
