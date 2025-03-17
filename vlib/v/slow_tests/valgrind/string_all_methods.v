6fb109cc2fc7b04d318c912d9493d3e010f9f73d
Alexander Medvednikov <alexander@medvednikov.com>
2025-03-17 16:35:10 +0300

    nice

---
+++ vlib/v/slow_tests/valgrind/string_all_methods.v
@@ -0,0 +1,6 @@
+fn main() {
+	'Hello World'.wrap(width: 10)
+	'Hello World'.wrap(width: 10, end: '<linea-break>')
+	'The V programming language'.wrap(width: 20, end: '|')
+	'Hello, my name is Carl and I am a delivery'.wrap(width: 20)
+}
