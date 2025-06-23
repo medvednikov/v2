0248af6bb6abec0e6b5a8e1959673ce6ef1ce541
Alexander Medvednikov <alexander@medvednikov.com>
2025-06-24 00:18:05 +0300

    veb2

---
+++ vlib/veb2/context.v
@@ -0,0 +1,283 @@
+module veb2
+
+import json
+import net
+import net.http
+import os
+
+enum ContextReturnType {
+	normal
+	file
+}
+
+pub enum RedirectType {
+	found              = int(http.Status.found)
+	moved_permanently  = int(http.Status.moved_permanently)
+	see_other          = int(http.Status.see_other)
+	temporary_redirect = int(http.Status.temporary_redirect)
+	permanent_redirect = int(http.Status.permanent_redirect)
+}
+
+// The Context struct represents the Context which holds the HTTP request and response.
+// It has fields for the query, form, files and methods for handling the request and response
+@[heap]
+pub struct Context {
+pub mut:
+	// TODO pub
+	// veb will try to infer the content type base on file extension,
+	// and if `content_type` is not empty the `Content-Type` header will always be
+	content_type string
+	// done is set to true when a response has been prepared
+	done bool
+	// if true, a warning will be logged. Connection takeover is not supported by this backend.
+	takeover bool
+	// These fields are kept for API compatibility but have limited function.
+	// return_type will be set, but the server does not stream files.
+	return_type           ContextReturnType = .normal
+	return_file           string
+	// if true the `Connection: close` header will be added to the response
+	client_wants_to_close bool
+	req               http.Request
+	custom_mime_types map[string]string
+	// TCP connection to client. Only for advanced usage!
+	conn &net.TcpConn = unsafe { nil }
+	// Map containing query params for the route.
+	// http://localhost:3000/index?q=vpm&order_by=desc => { 'q': 'vpm', 'order_by': 'desc' }
+	query map[string]string
+	// Multipart-form fields.
+	form map[string]string
+	// Files from multipart-form.
+	files map[string][]http.FileData
+	res   http.Response
+	// use form_error to pass errors from the context to your frontend
+	form_error                  string
+	livereload_poll_interval_ms int = 250 // Kept for API compatibility
+
+pub:
+	// time.ticks() from start of veb connection handle.
+	// You can use it to determine how much time is spent on your request.
+	page_gen_start i64
+}
+
+// returns the request header data from the key
+pub fn (ctx &Context) get_header(key http.CommonHeader) !string {
+	return ctx.req.header.get(key)!
+}
+
+// returns the request header data from the key
+pub fn (ctx &Context) get_custom_header(key string) !string {
+	return ctx.req.header.get_custom(key)!
+}
+
+// set a header on the response object
+pub fn (mut ctx Context) set_header(key http.CommonHeader, value string) {
+	ctx.res.header.set(key, value)
+}
+
+// set a custom header on the response object
+pub fn (mut ctx Context) set_custom_header(key string, value string) ! {
+	ctx.res.header.set_custom(key, value)!
+}
+
+// send_response_to_client finalizes the response headers and *prepares* the response object.
+// It NO LONGER sends data. The server loop sends the response after the handler returns.
+pub fn (mut ctx Context) send_response_to_client(mimetype string, response string) Result {
+	if ctx.done {
+		eprintln('[veb] a response cannot be sent twice over one connection')
+		return Result{}
+	}
+	ctx.done = true
+	ctx.res.body = response
+
+	// set Content-Type and Content-Length headers
+	mut custom_mimetype := if ctx.content_type.len == 0 { mimetype } else { ctx.content_type }
+	if custom_mimetype != '' {
+		ctx.res.header.set(.content_type, custom_mimetype)
+	}
+	if ctx.res.body != '' {
+		ctx.res.header.set(.content_length, ctx.res.body.len.str())
+	}
+	// send veb's closing headers
+	ctx.res.header.set(.server, 'veb')
+	if ctx.client_wants_to_close {
+		ctx.res.header.set(.connection, 'close')
+	}
+	// set the http version
+	ctx.res.set_version(.v1_1)
+	if ctx.res.status_code == 0 {
+		ctx.res.set_status(.ok)
+	}
+
+	return Result{}
+}
+
+// Response with payload and content-type `text/html`
+pub fn (mut ctx Context) html(s string) Result {
+	return ctx.send_response_to_client('text/html', s)
+}
+
+// Response with `s` as payload and content-type `text/plain`
+pub fn (mut ctx Context) text(s string) Result {
+	return ctx.send_response_to_client('text/plain', s)
+}
+
+// Response with json_s as payload and content-type `application/json`
+pub fn (mut ctx Context) json[T](j T) Result {
+	json_s := json.encode(j)
+	return ctx.send_response_to_client('application/json', json_s)
+}
+
+// Response with a pretty-printed JSON result
+pub fn (mut ctx Context) json_pretty[T](j T) Result {
+	json_s := json.encode_pretty(j)
+	return ctx.send_response_to_client('application/json', json_s)
+}
+
+// Response HTTP_OK with file as payload.
+// NOTE: This now reads the ENTIRE file into memory. It does not stream.
+pub fn (mut ctx Context) file(file_path string) Result {
+	if !os.exists(file_path) {
+		eprintln('[veb] file "${file_path}" does not exist')
+		return ctx.not_found()
+	}
+
+	ext := os.file_ext(file_path)
+
+	mut content_type := ctx.content_type
+	if content_type.len == 0 {
+		if ct := ctx.custom_mime_types[ext] {
+			content_type = ct
+		} else {
+			content_type = mime_types[ext]
+		}
+	}
+
+	if content_type.len == 0 {
+		eprintln('[veb] no MIME type found for extension "${ext}"')
+		return ctx.server_error('')
+	}
+
+	ctx.return_type = .file
+	ctx.return_file = file_path
+
+	// Read the entire file into memory and prepare it as a normal response.
+	data := os.read_file(file_path) or {
+		eprintln('[veb] error while trying to read file: ${err.msg()}')
+		return ctx.server_error('could not read resource')
+	}
+	return ctx.send_response_to_client(content_type, data)
+}
+
+// Response HTTP_OK with s as payload
+pub fn (mut ctx Context) ok(s string) Result {
+	mut mime := if ctx.content_type.len == 0 { 'text/plain' } else { ctx.content_type }
+	return ctx.send_response_to_client(mime, s)
+}
+
+// send an error 400 with a message
+pub fn (mut ctx Context) request_error(msg string) Result {
+	ctx.res.set_status(.bad_request)
+	return ctx.send_response_to_client('text/plain', msg)
+}
+
+// send an error 500 with a message
+pub fn (mut ctx Context) server_error(msg string) Result {
+	ctx.res.set_status(.internal_server_error)
+	return ctx.send_response_to_client('text/plain', msg)
+}
+
+// send an error with a custom status
+pub fn (mut ctx Context) server_error_with_status(s http.Status) Result {
+	ctx.res.set_status(s)
+	return ctx.send_response_to_client('text/plain', 'Server error')
+}
+
+// send a 204 No Content response without body and content-type
+pub fn (mut ctx Context) no_content() Result {
+	ctx.res.set_status(.no_content)
+	return ctx.send_response_to_client('', '')
+}
+
+@[params]
+pub struct RedirectParams {
+pub:
+	typ RedirectType
+}
+
+// Redirect to an url
+pub fn (mut ctx Context) redirect(url string, params RedirectParams) Result {
+	status := http.Status(params.typ)
+	ctx.res.set_status(status)
+
+	ctx.res.header.add(.location, url)
+	return ctx.send_response_to_client('text/plain', status.str())
+}
+
+// before_request is always the first function that is executed and acts as middleware
+pub fn (mut ctx Context) before_request() Result {
+	return Result{}
+}
+
+// returns a HTTP 404 response
+pub fn (mut ctx Context) not_found() Result {
+	ctx.res.set_status(.not_found)
+	return ctx.send_response_to_client('text/plain', '404 Not Found')
+}
+
+// Gets a cookie by a key
+pub fn (ctx &Context) get_cookie(key string) ?string {
+	if cookie := ctx.req.cookie(key) {
+		return cookie.value
+	} else {
+		return none
+	}
+}
+
+// Sets a cookie
+pub fn (mut ctx Context) set_cookie(cookie http.Cookie) {
+	cookie_raw := cookie.str()
+	if cookie_raw == '' {
+		eprintln('[veb] error setting cookie: name of cookie is invalid.\n${cookie}')
+		return
+	}
+	ctx.res.header.add(.set_cookie, cookie_raw)
+}
+
+// set_content_type sets the Content-Type header to `mime`
+pub fn (mut ctx Context) set_content_type(mime string) {
+	ctx.content_type = mime
+}
+
+// takeover_conn prevents veb from automatically sending a response and closing
+// the connection. You are responsible for closing the connection.
+// NOTE: True connection takeover for SSE/WebSockets is not supported by this server backend.
+// This function will only set a flag and the server will log a warning.
+pub fn (mut ctx Context) takeover_conn() {
+	ctx.takeover = true
+}
+
+// user_agent returns the user-agent header for the current client
+pub fn (ctx &Context) user_agent() string {
+	return ctx.req.header.get(.user_agent) or { '' }
+}
+
+// Returns the ip address from the current user
+pub fn (ctx &Context) ip() string {
+	mut ip := ctx.req.header.get_custom('CF-Connecting-IP') or { '' }
+	if ip == '' {
+		ip = ctx.req.header.get(.x_forwarded_for) or { '' }
+	}
+	if ip == '' {
+		ip = ctx.req.header.get_custom('X-Forwarded-For') or { '' }
+	}
+	if ip == '' {
+		ip = ctx.req.header.get_custom('X-Real-Ip') or { '' }
+	}
+	if ip.contains(',') {
+		ip = ip.all_before(',')
+	}
+	if ip == '' {
+		ip = ctx.conn.peer_ip() or { '' }
+	}
+	return ip
+}
---
+++ vlib/veb2/veb.v
@@ -0,0 +1,420 @@
+// Copyright (c) 2019-2024 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+//
+// This version is refactored to use `vanilla_http_server` exclusively.
+module veb2
+
+//import vanilla_http_server.http_server
+//import vanilla_http_server.request_parser
+import fasthttp
+import net
+import net.http
+import net.urllib
+import os
+import time
+import strings
+
+// A type which doesn't get filtered inside templates
+pub type RawHtml = string
+
+// A dummy structure that returns from routes to indicate that you actually sent something to a user
+@[noinit]
+pub struct Result {}
+
+// no_result does nothing, but returns `veb.Result`. Only use it when you are sure
+// a response will be sent over the connection, or in combination with `Context.takeoverP0+r\P0+r\P0+r\P0+r\P0+r\_conn`
+pub fn no_result() Result {
+	return Result{}
+}
+
+struct Route {
+	methods []http.Method
+	path    string
+	host    string
+mut:
+	middlewares       []voidptr
+	after_middlewares []voidptr
+}
+
+struct RequestParams {
+	global_app         voidptr
+	controllers_sorted        []&ControllerPath
+	routes             &map[string]Route
+	/*
+	timeout_in_seconds int
+mut:
+	// request body buffer
+	buf &u8 = unsafe { nil }
+	// idx keeps track of how much of the request body has been read
+	// for each incomplete request, see `handle_conn`
+	idx                 []int
+	incomplete_requests []http.Request
+	file_responses      []FileResponse
+	string_responses    []StringResponse
+	*/
+}
+
+// run - start a new veb server using the parallel vanilla_http_server backend.
+pub fn run[A, X](mut global_app A, port int) ! {
+//gapp = global_app
+	if port <= 0 || port > 65535 {
+		return error('invalid port number `${port}`, it should be between 1 and 65535')
+	}
+
+	// Generate routes and controllers just like the original run() function.
+	routes := generate_routes[A, X](global_app)!
+	controllers_sorted := check_duplicate_routes_in_controllers[A](global_app, routes)!
+
+	gparams = &RequestParams {
+	global_app: global_app
+	controllers_sorted:controllers_sorted
+	routes: &routes
+	//timeout_in_seconds: 
+	}
+
+
+	/*
+	// This closure is the "glue". It will be executed in parallel by worker threads
+	// for each incoming request.
+	request_handler := fn [mut global_app, routes, controllers_sorted](req_bytes []u8, client_fd int) ![]u8 {
+	}
+	*/
+
+	// Configure and run the vanilla_http_server.
+	mut server := fasthttp.Server{
+		port: port
+		request_handler: kek_handler[A,X]
+	}
+	println('[veb] Running multi-threaded app on http://localhost:${port}/')
+	flush_stdout()
+	server.run()
+}
+
+
+//__global gapp voidptr
+__global gparams RequestParams
+
+const http_ok_response = 'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 0\r\nConnection: keep-alive\r\n\r\n'.bytes()
+
+fn kek_handler[A, X](req fasthttp.HttpRequest) ![]u8 {
+	//println('kek_handler')
+
+	// println('handle_request() params.routes=${params.routes}')
+	//mut global_app := unsafe { &A(params.global_app) }
+	//
+	//mut global_app := unsafe { &A(gapp) }
+	mut global_app := unsafe { &A(gparams.global_app) }
+
+	//println('global_app=$global_app')
+
+	//println('params=$gparams')
+
+	//println('req=$req')
+
+	//println('buffer=${req.buffer.bytestr()}')
+	s := req.buffer.bytestr()
+	
+
+
+	method := unsafe { tos(&req.buffer[req.method.start], req.method.len) }
+	path := unsafe { tos(&req.buffer[req.path.start], req.path.len) }
+
+	req_bytes := req.buffer
+	client_fd:=req.client_conn_fd
+
+	/*
+
+		// 1. Parse the raw request bytes into a standard `http.Request`.
+		req2 := http.parse_request(req_bytes) or {
+			eprintln('[veb] Failed to parse request: ${err}')
+			return http_server.tiny_bad_request_response
+		}
+		*/
+
+	       //println('OLOO')
+		req2:=http.parse_request_head_str(s) or {
+			eprintln('[veb] Failed to parse request: ${err}')
+			//println('s=')
+			//println(s)
+			return http_ok_response// http_server.tiny_bad_request_response
+			}
+
+
+		//println('parsed req: $req')
+
+		// 2. Create and populate the `veb.Context`.
+		completed_context := handle_request_and_route[A, X](mut global_app, req2, client_fd, gparams.routes, gparams.controllers_sorted)
+
+		// 3. Serialize the final `http.Response` into a byte array.
+		// Check for limitations of this synchronous backend.
+		if completed_context.takeover {
+			eprintln('[veb] WARNING: ctx.takeover_conn() was called, but this is not supported by this server backend. The connection will be closed after this response.')
+		}
+
+		// The vanilla_http_server expects a complete response buffer to be returned.
+		return completed_context.res.bytes()
+
+
+
+
+
+
+	//return  http_ok_response
+	}
+
+
+// handle_request_and_route is a unified function that creates the context,
+// runs middleware, and finds the correct route for a request.
+fn handle_request_and_route[A, X](mut app A, req http.Request, client_fd int, routes &map[string]Route, controllers []&ControllerPath) &Context {
+
+
+
+	// Create a `net.TcpConn` from the file descriptor for context compatibility.
+	mut conn := &net.TcpConn{
+		sock:        net.tcp_socket_from_handle_raw(client_fd)
+		handle:      client_fd
+		is_blocking: false // vanilla_http_server ensures this
+	}
+
+	// Create and populate the `veb.Context` from the request.
+	mut url := urllib.parse(req.url) or {
+		// This should be rare if http.parse_request succeeded.
+		mut bad_ctx := &Context{req: req, conn: conn}
+		bad_ctx.not_found()
+		return bad_ctx
+	}
+	query := parse_query_from_url(url)
+	form, files := parse_form_from_request(req) or {
+		mut bad_ctx := &Context{req: req, conn: conn}
+		bad_ctx.request_error('Failed to parse form data: ${err.msg()}')
+		return bad_ctx
+	}
+	host_with_port := req.header.get(.host) or { '' }
+	host, _ := urllib.split_host_port(host_with_port)
+
+	mut ctx := &Context{
+		req:            req
+		page_gen_start: time.ticks()
+		conn:           conn
+		query:          query
+		form:           form
+		files:          files
+	}
+
+	if connection_header := req.header.get(.connection) {
+		if connection_header.to_lower() == 'close' {
+			ctx.client_wants_to_close = true
+		}
+	}
+
+	$if A is StaticApp {
+		ctx.custom_mime_types = app.static_mime_types.clone()
+	}
+
+	// Match controller paths first
+	$if A is ControllerInterface {
+		if completed_context := handle_controllers[X](controllers, ctx, mut url, host) {
+			return completed_context
+		}
+	}
+
+	// Create a new user context and pass veb's context
+	mut user_context := X{}
+	user_context.Context = ctx
+
+	handle_route[A, X](mut app, mut user_context, url, host, routes)
+	return &user_context.Context
+}
+
+// handle_route finds and executes the correct handler for a given URL.
+// This is adapted from the original `veb` but with simplified cleanup.
+fn handle_route[A, X](mut app A, mut user_context X, url urllib.URL, host string, routes &map[string]Route) {
+////println('handle_route() url=$url host=$host')
+//println('AZAZA routes=$routes')
+	mut route := Route{}
+	mut middleware_has_sent_response := false
+	mut not_found := false
+
+	defer {
+		// After-middleware logic
+		if !not_found && !middleware_has_sent_response {
+			was_done := user_context.Context.done
+			user_context.Context.done = false
+			$if A is MiddlewareApp {
+				validate_middleware[X](mut user_context, app.Middleware.get_global_handlers_after[X]())
+				validate_middleware[X](mut user_context, route.after_middlewares)
+			}
+					//println('user_context2=${user_context} was_done=${was_done}')
+			if !was_done && !user_context.Context.done {
+				eprintln('[veb] handler for route "${url.path}" does not send any data! LOL')
+				user_context.server_error('Handler did not produce a response.')
+			}
+		}
+	}
+
+	url_words := url.path.split('/').filter(it != '')
+
+	$if A is HasBeforeRequest {
+		app.before_request()
+	}
+	if user_context.Context.done {
+		return
+	}
+
+	$if A is MiddlewareApp {
+		if !validate_middleware[X](mut user_context, app.Middleware.get_global_handlers[X]()) {
+			middleware_has_sent_response = true
+			return
+		}
+	}
+
+	$if A is StaticApp {
+		if serve_if_static[A, X](app, mut user_context, url, host) {
+			return
+		}
+	}
+
+	// Route matching logic (identical to original)
+	//println("FINDING ROUTE")
+	$for method in A.methods {
+		//println('method $method.name')
+		$if method.return_type is Result {
+			route = (*routes)[method.name] or { return }
+			if user_context.Context.req.method in route.methods {
+				route_words := route.path.split('/').filter(it != '')
+				if route.host == '' || route.host == host {
+					can_have_data_args := user_context.Context.req.method in methods_with_form
+					if !route.path.contains('/:') && url_words == route_words {
+						if !validate_middleware[X](mut user_context, route.middlewares) {
+							middleware_has_sent_response = true
+							return
+						}
+						if method.args.len > 1 && can_have_data_args {
+							data := if user_context.Context.req.method == .get { user_context.Context.query } else { user_context.Context.form }
+							mut args := []string{cap: method.args.len + 1}
+							for param in method.args[1..] {
+								args << data[param.name]
+							}
+							//println('FOUND 1')
+							app.$method(mut user_context, args)
+						} else {
+							//println('FOUND 2 $method.name')
+							app.$method(mut user_context)
+							//println('END OF METHOD CALLED')
+						}
+						return
+					}
+					if url_words.len == 0 && route_words == ['index'] && method.name == 'index' {
+						if !validate_middleware[X](mut user_context, route.middlewares) {
+							middleware_has_sent_response = true
+							return
+						}
+							//println('FOUND 3')
+						app.$method(mut user_context)
+						return
+					}
+					if params := route_matches(url_words, route_words) {
+						if !validate_middleware[X](mut user_context, route.middlewares) {
+							middleware_has_sent_response = true
+							return
+						}
+							//println('FOUND 4')
+						app.$method(mut user_context, params)
+						return
+					}
+				}
+			}
+		}
+	}
+	//println('not found')
+
+	user_context.not_found()
+	not_found = true
+}
+
+// --- Helper functions (kept from original) ---
+
+fn generate_routes[A, X](app &A) !map[string]Route {
+	mut routes := map[string]Route{}
+	$for method in A.methods {
+		$if method.return_type is Result {
+			http_methods, route_path, host := parse_attrs(method.name, method.attrs) or {
+				return error('error parsing method attributes: ${err}')
+			}
+			mut route := Route{
+				methods: http_methods
+				path:    route_path
+				host:    host
+			}
+			$if A is MiddlewareApp {
+				route.middlewares = app.Middleware.get_handlers_for_route[X](route_path)
+				route.after_middlewares = app.Middleware.get_handlers_for_route_after[X](route_path)
+			}
+			routes[method.name] = route
+		}
+	}
+	return routes
+}
+
+fn route_matches(url_words []string, route_words []string) ?[]string {
+	if route_words.len == 1 && route_words[0].starts_with(':') && route_words[0].ends_with('...') {
+		return ['/' + url_words.join('/')]
+	}
+	if url_words.len < route_words.len {
+		return none
+	}
+	mut params := []string{cap: url_words.len}
+	if url_words.len == route_words.len {
+		for i in 0 .. url_words.len {
+			if route_words[i].starts_with(':') {
+				params << url_words[i]
+			} else if route_words[i] != url_words[i] {
+				return none
+			}
+		}
+		return params
+	}
+	if route_words.len == 0 || !route_words[route_words.len - 1].ends_with('...') {
+		return none
+	}
+	for i in 0 .. route_words.len - 1 {
+		if route_words[i].starts_with(':') {
+			params << url_words[i]
+		} else if route_words[i] != url_words[i] {
+			return none
+		}
+	}
+	params << url_words[route_words.len - 1..url_words.len].join('/')
+	return params
+}
+
+fn serve_if_static[A, X](app &A, mut user_context X, url urllib.URL, host string) bool {
+	mut asked_path := url.path
+	base_path := os.base(asked_path)
+	if !base_path.contains('.') && !asked_path.ends_with('/') {
+		asked_path += '/'
+	}
+	if asked_path.ends_with('/') {
+		if app.static_files[asked_path + 'index.html'] != '' {
+			asked_path += 'index.html'
+		} else if app.static_files[asked_path + 'index.htm'] != '' {
+			asked_path += 'index.htm'
+		}
+	}
+	static_file := app.static_files[asked_path] or { return false }
+	ext := os.file_ext(static_file).to_lower()
+	mut mime_type := app.static_mime_types[ext] or { mime_types[ext] }
+	static_host := app.static_hosts[asked_path] or { '' }
+	if static_file == '' || mime_type == '' || (static_host != '' && static_host != host) {
+		return false
+	}
+	user_context.file(static_file)
+	return true
+}
+
+// Set s to the form error
+pub fn (mut ctx Context) error(s string) {
+	eprintln('[veb] Context.error: ${s}')
+	ctx.form_error = s
+}
