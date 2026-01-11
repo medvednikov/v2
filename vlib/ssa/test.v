c3d9d9e2c9e453944bd4e6073f2caafdb2777859
Alexander Medvednikov <alexander@vlang.io>
2026-01-11 09:28:08 +0300

    better test

--- vlib/ssa/test.v
+++ vlib/ssa/test.v
@@ -1,19 +1,51 @@
-fn add(a int, b int) int {
-	res := a + b
-	return res
+struct Point {
+	x int
+	y int
 }
 
+__global (
+	g_val int
+)
+
 fn main() {
-	for i := 0; i < 10; i++ {
-		j := 777
+	// 1. Struct Decl & Init & SelectorExpr
+	p := Point{x: 10, y: 20}
+	
+	// 2. Selector Assign (L-Value)
+	p.x = 100
+	
+	// 3. Globals & Compound Assign
+	g_val = 5
+	g_val += 5
+	
+	// 4. IndexExpr & Pointers
+	// Simulate array via pointer arithmetic logic in Builder/Backend
+	// Since we don't have malloc in this MVP, we use the struct pointer as a base
+	// or just a stack variable address treated as array base.
+	// For valid C output, let's use the struct ptr cast logic implicitly handled.
+	
+	// 5. Bool & Logic
+	flag := true
+	if flag {
+		p.y = 200
 	}
-	x := 10
-	if x > 10 {
-		b := 20
-	} else {
-		b := 21
+
+	// 6. Loop with Break/Continue
+	i := 0
+	sum := 0
+	for i < 10 {
+		i++
+		if i == 5 {
+			continue
+		}
+		if i > 8 {
+			break
+		}
+		sum += i
 	}
-	y := 20
-	z := add(x, y)
-	return z
+	
+	// 7. Cast (implicit in this backend)
+	val := int(sum)
+	
+	return val
 }
