4522ed55536bfc24d44cd4b7a7ebbf8ffc595b2a
Alexander Medvednikov <alexander@vlang.io>
2026-01-15 04:58:23 +0300

    fmt

--- vlib/ssa/backend/arm64.v
+++ vlib/ssa/backend/arm64.v
@@ -7,12 +7,12 @@ pub struct Arm64Gen {
 	mod &ssa.Module
 mut:
 	macho &MachOObject
-	
-	stack_map   map[int]int 
+
+	stack_map   map[int]int
 	stack_size  int
-	curr_offset int 
-	
-	block_offsets map[int]int
+	curr_offset int
+
+	block_offsets  map[int]int
 	pending_labels map[int][]int
 }
 
@@ -27,9 +27,11 @@ pub fn (mut g Arm64Gen) gen() {
 	for func in g.mod.funcs {
 		g.gen_func(func)
 	}
-	
+
 	// Globals (aligned 8)
-	for g.macho.text_data.len % 8 != 0 { g.emit(0xd503201f) }
+	for g.macho.text_data.len % 8 != 0 {
+		g.emit(0xd503201f)
+	}
 	for gvar in g.mod.globals {
 		addr := u64(g.macho.text_data.len)
 		g.macho.add_symbol('_' + gvar.name, addr, true, 1)
@@ -43,7 +45,7 @@ fn (mut g Arm64Gen) gen_func(func ssa.Function) {
 	g.stack_map = map[int]int{}
 	g.block_offsets = map[int]int{}
 	g.pending_labels = map[int][]int{}
-	
+
 	// Stack frame calculation
 	mut val_count := 0
 	for blk_id in func.blocks {
@@ -51,14 +53,14 @@ fn (mut g Arm64Gen) gen_func(func ssa.Function) {
 	}
 	val_count += func.params.len
 	g.stack_size = (val_count * 8 + 16) & ~0xF
-	
+
 	g.macho.add_symbol('_' + func.name, u64(g.curr_offset), true, 1)
-	
+
 	// Prologue
 	g.emit(0xA9BF7BFD) // stp fp, lr, [sp, -16]!
 	g.emit(0x910003FD) // mov fp, sp
 	g.emit_sub_sp(g.stack_size)
-	
+
 	mut slot_offset := 8
 	for i, pid in func.params {
 		offset := -slot_offset
@@ -72,24 +74,26 @@ fn (mut g Arm64Gen) gen_func(func ssa.Function) {
 		blk := g.mod.blocks[blk_id]
 		for val_id in blk.instrs {
 			val := g.mod.values[val_id]
-			if val.kind != .instruction { continue }
+			if val.kind != .instruction {
+				continue
+			}
 			offset := -slot_offset
 			g.stack_map[val_id] = offset
 			slot_offset += 8
 		}
 	}
-	
+
 	for blk_id in func.blocks {
 		blk := g.mod.blocks[blk_id]
 		g.block_offsets[blk_id] = g.macho.text_data.len - g.curr_offset
-		
+
 		if offsets := g.pending_labels[blk_id] {
 			for off in offsets {
 				target := g.block_offsets[blk_id]
 				rel := (target - off) / 4
 				abs_off := g.curr_offset + off
 				instr := g.read_u32(abs_off)
-				
+
 				mut new_instr := u32(0)
 				if (instr & 0xFC000000) == 0x14000000 {
 					new_instr = (instr & 0xFC000000) | (u32(rel) & 0x3FFFFFF)
@@ -99,7 +103,7 @@ fn (mut g Arm64Gen) gen_func(func ssa.Function) {
 				g.write_u32(abs_off, new_instr)
 			}
 		}
-		
+
 		for val_id in blk.instrs {
 			g.gen_instr(val_id)
 		}
@@ -108,16 +112,22 @@ fn (mut g Arm64Gen) gen_func(func ssa.Function) {
 
 fn (mut g Arm64Gen) gen_instr(val_id int) {
 	instr := g.mod.instrs[g.mod.values[val_id].index]
-	
+
 	match instr.op {
 		.add, .sub, .mul, .sdiv, .eq, .ne, .lt, .gt, .le, .ge {
 			g.load_val_to_reg(8, instr.operands[0])
 			g.load_val_to_reg(9, instr.operands[1])
-			
+
 			match instr.op {
-				.add { g.emit(0x8B090108) }
-				.sub { g.emit(0xCB090108) }
-				.mul { g.emit(0x9B097D08) }
+				.add {
+					g.emit(0x8B090108)
+				}
+				.sub {
+					g.emit(0xCB090108)
+				}
+				.mul {
+					g.emit(0x9B097D08)
+				}
 				.eq, .ne, .lt, .gt, .le, .ge {
 					g.emit(0xEB09011F) // cmp
 					code := match instr.op {
@@ -158,19 +168,23 @@ fn (mut g Arm64Gen) gen_instr(val_id int) {
 		}
 		.call {
 			for i in 1 .. instr.operands.len {
-				if i-1 < 8 { g.load_val_to_reg(i-1, instr.operands[i]) }
+				if i - 1 < 8 {
+					g.load_val_to_reg(i - 1, instr.operands[i])
+				}
 			}
 			fn_val := g.mod.values[instr.operands[0]]
 			sym_idx := g.macho.add_undefined('_' + fn_val.name)
-			g.macho.add_reloc(g.macho.text_data.len, sym_idx, backend.arm64_reloc_branch26, true)
+			g.macho.add_reloc(g.macho.text_data.len, sym_idx, arm64_reloc_branch26, true)
 			g.emit(0x94000000)
-			
+
 			if g.mod.type_store.types[g.mod.values[val_id].typ].kind != .void_t {
 				g.store_reg_to_val(0, val_id)
 			}
 		}
 		.ret {
-			if instr.operands.len > 0 { g.load_val_to_reg(0, instr.operands[0]) }
+			if instr.operands.len > 0 {
+				g.load_val_to_reg(0, instr.operands[0])
+			}
 			g.emit(0x910003BF)
 			g.emit(0xA8C17BFD)
 			g.emit(0xD65F03C0)
@@ -189,10 +203,10 @@ fn (mut g Arm64Gen) gen_instr(val_id int) {
 		.br {
 			g.load_val_to_reg(8, instr.operands[0])
 			g.emit(0xF100011F) // cmp x8, 0
-			
+
 			true_blk := g.mod.values[instr.operands[1]].index
 			false_blk := g.mod.values[instr.operands[2]].index
-			
+
 			if off := g.block_offsets[true_blk] {
 				rel := (off - (g.macho.text_data.len - g.curr_offset)) / 4
 				g.emit(0x54000001 | ((u32(rel) & 0x7FFFF) << 5))
@@ -200,7 +214,7 @@ fn (mut g Arm64Gen) gen_instr(val_id int) {
 				g.record_pending_label(true_blk)
 				g.emit(0x54000001)
 			}
-			
+
 			if off := g.block_offsets[false_blk] {
 				rel := (off - (g.macho.text_data.len - g.curr_offset)) / 4
 				g.emit(0x14000000 | (u32(rel) & 0x3FFFFFF))
@@ -221,11 +235,12 @@ fn (mut g Arm64Gen) load_val_to_reg(reg int, val_id int) {
 			str_offset := g.macho.str_data.len
 			g.macho.str_data << str_content.bytes()
 			g.macho.str_data << 0
-			
-			sym_idx := g.macho.add_symbol('L_str_${str_offset}', u64(str_offset), false, 2)
-			g.macho.add_reloc(g.macho.text_data.len, sym_idx, backend.arm64_reloc_page21, true)
+
+			sym_idx := g.macho.add_symbol('L_str_${str_offset}', u64(str_offset), false,
+				2)
+			g.macho.add_reloc(g.macho.text_data.len, sym_idx, arm64_reloc_page21, true)
 			g.emit(0x90000000 | u32(reg))
-			g.macho.add_reloc(g.macho.text_data.len, sym_idx, backend.arm64_reloc_pageoff12, false)
+			g.macho.add_reloc(g.macho.text_data.len, sym_idx, arm64_reloc_pageoff12, false)
 			g.emit(0x91000000 | u32(reg) | (u32(reg) << 5))
 		} else {
 			int_val := val.name.int()
@@ -233,9 +248,11 @@ fn (mut g Arm64Gen) load_val_to_reg(reg int, val_id int) {
 		}
 	} else if val.kind == .global {
 		sym_idx := g.macho.add_undefined('_' + val.name)
-		g.macho.add_reloc(g.macho.text_data.len, sym_idx, backend.arm64_reloc_got_load_page21, true)
+		g.macho.add_reloc(g.macho.text_data.len, sym_idx, arm64_reloc_got_load_page21,
+			true)
 		g.emit(0x90000000 | u32(reg))
-		g.macho.add_reloc(g.macho.text_data.len, sym_idx, backend.arm64_reloc_got_load_pageoff12, false)
+		g.macho.add_reloc(g.macho.text_data.len, sym_idx, arm64_reloc_got_load_pageoff12,
+			false)
 		g.emit(0xF9400000 | u32(reg) | (u32(reg) << 5))
 	} else {
 		offset := g.stack_map[val_id]
@@ -276,11 +293,11 @@ fn (mut g Arm64Gen) record_pending_label(blk int) {
 }
 
 fn (g Arm64Gen) read_u32(off int) u32 {
-	return binary.little_endian_u32(g.macho.text_data[off..off+4])
+	return binary.little_endian_u32(g.macho.text_data[off..off + 4])
 }
 
 fn (mut g Arm64Gen) write_u32(off int, v u32) {
-	binary.little_endian_put_u32(mut g.macho.text_data[off..off+4], v)
+	binary.little_endian_put_u32(mut g.macho.text_data[off..off + 4], v)
 }
 
 pub fn (mut g Arm64Gen) write_file(path string) {
