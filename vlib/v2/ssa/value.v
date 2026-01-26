d9100d38392c921d7a3e3edca84fb18c86276fb9
Alexander Medvednikov <alexander@vlang.io>
2026-01-26 06:16:52 +0300

    new v2

---
+++ vlib/v2/ssa/value.v
@@ -0,0 +1,40 @@
+module ssa
+
+pub type ValueID = int
+
+pub enum ValueKind {
+	unknown
+	constant
+	argument
+	global
+	instruction
+	basic_block
+}
+
+pub struct Value {
+pub:
+	id  ValueID
+	typ TypeID
+	// Index into the specific arena (instrs, blocks, globals)
+	index int
+pub mut:
+	kind ValueKind
+	name string
+	uses []ValueID
+}
+
+pub struct ConstantData {
+pub:
+	int_val   i64
+	float_val f64
+	str_val   string
+}
+
+pub struct GlobalVar {
+pub:
+	name        string
+	typ         TypeID
+	linkage     Linkage
+	alignment   int
+	is_constant bool
+}
