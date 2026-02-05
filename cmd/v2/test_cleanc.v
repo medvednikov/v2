a3c1ebf691255dae831cd5d778357688deefe085
Alexander Medvednikov <alexander@medvednikov.com>
2026-02-05 16:52:28 +0300

    testcleancv

---
+++ cmd/v2/test_cleanc.v
@@ -0,0 +1,47 @@
+fn add(a int, b int) int {
+	return a + b
+}
+
+fn sub(a int, b int) int {
+	return a - b
+}
+
+fn mul(a int, b int) int {
+	return a * b
+}
+
+fn main() {
+	// 1. Basic variable declarations and arithmetic
+	x := 3
+	y := 2
+	z := x + y
+	println(z) // 5
+
+	// 2. Function calls
+	a := add(10, 20)
+	println(a) // 30
+
+	// 3. Nested function calls
+	b := add(mul(3, 4), sub(10, 5))
+	println(b) // 17
+
+	// 4. Compound expressions
+	c := (x + y) * 2
+	println(c) // 10
+
+	// 5. Multiple variables and operations
+	d := 100
+	e := d - 42
+	println(e) // 58
+
+	// 6. Boolean / comparison (int result)
+	f := 10
+	g := 20
+	if f < g {
+		println(1)
+	}
+	// 1
+
+	// 7. String literal
+	println('hello')
+}
