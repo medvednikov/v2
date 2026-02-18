2f68442626d520c69c7ea366f13d22439b5959de
Alexander Medvednikov <alexander@medvednikov.com>
2026-02-18 07:02:34 +0300

    t fix

--- vlib/v2/transformer/types.v
+++ vlib/v2/transformer/types.v
@@ -334,6 +334,9 @@ fn (t &Transformer) get_var_type_name(name string) string {
 	if typ is types.Struct {
 		return typ.name
 	}
+	if typ is types.SumType {
+		return types.sum_type_name(typ)
+	}
 	if typ is types.Pointer {
 		base_name := t.type_to_name(typ.base_type)
 		if base_name != '' {
